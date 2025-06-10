d22ae91c8e4fa7991d51a58bea838c3b5484aad7
dried021 <dried021@gmail.com>
2025-06-10 17:48:56 +0900

    back

---
+++ jigubangbang/src/main/java/com/jigubangbang/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.bookfox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/profile/*")
+                .addResourceLocations("file:uploads/profile-images/");
+    }
+}
---
+++ jigubangbang/src/test/java/com/jigubangbang/jigubangbang/JigubangbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigubangbang.jigubangbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigubangbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
